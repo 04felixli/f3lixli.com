99dd96ad17562a809434301372e5cc83834a95ee
Felix Li <04felix.li@gmail.com>
2024-07-19 13:05:48 -0400

    Typewriter effect responsive

--- app/components/About/Name.tsx
+++ app/components/About/Name.tsx
@@ -1,13 +1,15 @@
 "use client"
 import { AboutMe } from '@/app/Objects/AboutMeObjects'
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 import { TypeAnimation } from 'react-type-animation'
 
 const Name = () => {
+    // const isMobile = useMediaQuery({ query: '(max-width: 320px)' });
     return (
         <div>
             <p className="text-3xl mt-5">I'm</p>
-            <p className="text-7xl border-b-2 w-full">
+            <p className="text-4xl max-[265px]:hidden min-[489px]:text-5xl min-[490px]:max-[589px]:text-6xl min-[589px]:text-7xl border-b-2 w-full max-[1124px]:h-36 font-semibold">
                 <TypeAnimation
                     sequence={[
                         1000,
@@ -16,7 +18,21 @@ const Name = () => {
                         1000, // wait 1s before replacing "Mice" with "Hamsters"
                         "A Software Developer",
                         1000,
-                        "A Full-Stack Developer",
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style={{ display: 'inline-block' }}
+                    repeat={Infinity}
+                />
+            </p>
+            <p className="text-4xl min-[265px]:hidden font-semibold border-b-2 w-full">
+                <TypeAnimation
+                    sequence={[
+                        1000,
+                        // Same substring at the start will only be typed out once, initially
+                        'Felix Li',
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        "",
                         1000,
                     ]}
                     wrapper="span"
@@ -26,7 +42,6 @@ const Name = () => {
                 />
             </p>
         </div>
-
     )
 }
 
