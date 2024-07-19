ee3afbc3479cd0fd2bc18ccb015fd968a273ecd5
Felix Li <04felix.li@gmail.com>
2024-07-19 10:23:55 -0400

    Consistent page load animation

--- app/components/util/SectionLayout.tsx
+++ app/components/util/SectionLayout.tsx
@@ -3,16 +3,6 @@ import { motion } from 'framer-motion';
 import { MotionDiv } from './MotionDiv';
 import SectionTitle from './SectionTitle';
 
-const titleVariants = {
-    hidden: { x: '-100%', opacity: 0 },
-    visible: { x: 0, opacity: 1 }
-};
-
-const contentVariants = {
-    hidden: { x: '100%', opacity: 0 },
-    visible: { x: 0, opacity: 1 }
-};
-
 interface Props {
     children: ReactNode;
     sectionName: string;
@@ -22,28 +12,18 @@ const SectionLayout = ({ children, sectionName }: Props) => {
     return (
         <section className='flex items-center w-full flex-col'>
             <MotionDiv
-                variants={titleVariants}
-                initial="hidden"
-                animate="visible"
-                transition={{
-                    delay: 0.1,
-                    ease: "easeInOut",
-                    duration: 0.5
-                }}
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
                 viewport={{ amount: 0 }}
                 className='w-full'
             >
                 <SectionTitle sectionName={sectionName} />
             </MotionDiv>
             <MotionDiv
-                variants={contentVariants}
-                initial="hidden"
-                animate="visible"
-                transition={{
-                    delay: 0.1,
-                    ease: "easeInOut",
-                    duration: 0.5
-                }}
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.4, duration: 0.5 }}
                 viewport={{ amount: 0 }}
                 className='w-full'
             >
