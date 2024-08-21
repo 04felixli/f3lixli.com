5767467ae5c0858c6e5f12ae9299fe1109133de7
Felix Li <04felix.li@gmail.com>
2024-08-21 14:05:27 -0400

    Add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Titillium_Web } from "next/font/google";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const font = Titillium_Web({
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`max-w-screen min-w-screen min-h-screen flex justify-center ${font.className}`}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
