172c41ed2c5591fe2e19e6dc08f1862669ecf79a
Felix Li <04felix.li@gmail.com>
2024-09-05 20:19:23 -0400

    Update Experiences

--- app/experiences/USRA.tsx
+++ app/experiences/USRA.tsx
@@ -7,16 +7,16 @@ const Rogers = () => {
         <ExperienceCard experience={USRAExperience}>
             <ul className='list-disc list-outside ml-5'>
                 <li>
-                    Contributed to a PhD student&#39;s research on estabilishing a pipeline to generate groundtruths for <span className='font-bold'>template based log
-                        parsing</span> of large scale log data from various software systems under the supervision of Dr. W. Shang.
-                </li>
-                <li>
-                    Generated a user study for <span className='font-bold'>20 participants</span> to identify discrepancies between automatically generated log templates and
-                    user perceptions, leading to improved log parsing techniques.
+                    Conducted research to optimize log analysis of large scale log data from various software systems by establishing a
+                    pipeline to generate ground truths for <span className='font-bold'>template based log parsing</span>.
                 </li>
                 <li>
                     Saved <span className='font-bold'>10+ hours weekly</span> by developing <span className='font-bold'>Python scripts</span> to analyze over <span className='font-bold'>2 million</span> log entries and templates from <span className='font-bold'>12 </span>
                     different software systems, streamlining data collection for the study.
+                </li>
+                <li>
+                    Generated a user study for <span className='font-bold'>20 participants</span> to identify discrepancies between automatically generated log templates and
+                    user perceptions, leading to improved log parsing techniques.
                 </li>
             </ul>
         </ExperienceCard>
