1bd3d24509698c2ba8fac4baf11d769d1e309d3e
04felixli <04felix.li@gmail.com>
2025-02-01 20:59:48 -0500

    Update home page

--- app/Objects/AboutMeObjects.tsx
+++ app/Objects/AboutMeObjects.tsx
@@ -9,7 +9,7 @@ interface AboutMeObject {
 
 export const AboutMe: AboutMeObject = {
     name: "Felix Li",
-    description: "Hi! I'm a Computer Engineering student at the University of Waterloo, diving deep into the world of code and coffee, and actively seeking winter 2025 internships. \n\nWhen I'm not studying or working, you can find me at the gym, playing basketball or swimming. Let's create something awesome together!",
+    description: "Hi! I'm a Computer Engineering student at the University of Waterloo, diving deep into the world of code and coffee, and actively seeking internship opportunities. \n\nWhen I'm not studying or working, you can find me at the gym, playing basketball or swimming. Let's create something awesome together!",
     pfpPath: "/Images/Pfp.jpeg",
     linkedIn: 'https://www.linkedin.com/in/04felixli',
     github: 'https://github.com/04felixli',
